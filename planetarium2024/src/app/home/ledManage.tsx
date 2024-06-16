2b8d9f659faf4a48835fec9022af3eef7e7bb661
pi-starsphere <tokyotech.astroclub@gmail.com>
2024-06-16 23:49:00 +0900

    chore: route削除

--- planetarium2024/src/app/home/ledManage.tsx
+++ planetarium2024/src/app/home/ledManage.tsx
@@ -1,5 +1,5 @@
 import { FormGroup, ButtonGroup, Button, Box } from "@mui/material"
-import leds from '@/LEDs.json'
+import leds from '@/leds.json'
 
 export default function LEDList() {
   return (
