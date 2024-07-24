d27a7e8351b4ae4888d42721bf7c792f4f584f08
pi-controller <tokyotech.astroclub@gmail.com>
2024-07-25 04:32:28 +0900

    add: memo_alsa.txt

--- planetarium2024/apiServerAudio/launch.sh
+++ planetarium2024/apiServerAudio/launch.sh
@@ -1,1 +1,4 @@
-uvicorn main:app --host pi-controller.local --port 8001 --reload
+#!/usr/bin/sh
+
+cd `dirname $0`
+/home/pi/.local/bin/uvicorn main:app --host pi-controller.local --port 8001
--- planetarium2024/apiServerLED/launch.sh
+++ planetarium2024/apiServerLED/launch.sh
@@ -1,1 +1,4 @@
-uvicorn main:app --host pi-starsphere.local --port 8000 --reload
+#!/usr/bin/sh
+
+cd `dirname $0`
+/home/pi/.local/bin/uvicorn main:app --host pi-starsphere.local --port 8000
