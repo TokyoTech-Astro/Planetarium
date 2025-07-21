f1ed72e7d7fa1e4ed7f268920aafe53a1ef52198
rin <odanagi02@gmail.com>
2025-07-21 21:08:33 +0900

    chore: ファイル整理

--- planetarium2023/autorun_controller.sh
+++ planetarium2023/autorun_controller.sh
@@ -1,4 +1,4 @@
 #!usr//bin/bash
+
 sudo iwconfig wlan0 power off
-cd ./planetarium2023
 python3 client_motor.py
--- planetarium2023/autorun_starsphere.sh
+++ planetarium2023/autorun_starsphere.sh
@@ -1,4 +1,7 @@
 #!usr//bin/sh
+
+#2023年のプラネタリウムの自動起動スクリプト
+
 sudo iwconfig wlan0 power off
 cd ./planetarium2023
 python3 server_starsphere.py
